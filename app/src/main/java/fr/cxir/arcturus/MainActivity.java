7e04996b4095f7bde15c86ce089b1e4ac01437ad
CXIR <cxir.developer@gmail.com>
2018-07-04 19:47:16 +0200

    Commit 1, July 3
    Maps first implementation

--- app/src/main/java/fr/cxir/arcturus/MainActivity.java
+++ app/src/main/java/fr/cxir/arcturus/MainActivity.java
@@ -1,98 +1,40 @@
 package fr.cxir.arcturus;
 
-import android.app.Fragment;
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.ArrayMap;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
-import java.util.HashMap;
 
-import fr.cxir.arcturus.Fragments.CartFragment;
-import fr.cxir.arcturus.Fragments.SettingsFragment;
-import fr.cxir.arcturus.Tasks.GetProductsTask;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    private HashMap< String, Object > fragmentsMap = new HashMap<>();
-
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        navigationInitialization();
 
+        this.button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMaps();
+            }
+        });
     }
 
-    private void launchAccountCreation() {
-        Intent intent = new Intent(this,CreateAccountActivity.class);
+    private void launchMaps() {
+
+        Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if(id == R.id.about){
-            launchAccountCreation();
-        }
-
-        //launchFragment( );
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    private void navigationInitialization(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    private void launchFragment(Fragment fragment){
-
-        getFragmentManager()
-                .beginTransaction()
-                .add(R.id.content, fragment)
-                .commit();
-    }
 
 
 }
